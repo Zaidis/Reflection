2ad1c96fe2661929298d6759d8c8643723d0436f
Zaidis <chewybinx@gmail.com>
2021-08-14 22:29:45 -0500

    sd

---
+++ Reflection/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+
+    public float sensitivity = 100f;
+    public Transform body;
+    float xRotation = 0f;
+    float x, y;
+
+    bool gameStarted;
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        //after 38 seconds (after the boom sound) we move the camera to the player
+        Invoke("TransitionCamera", 38f);
+    }
+    private void Update() {
+        if (gameStarted) {
+            x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+            xRotation -= y;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            body.Rotate(Vector3.up * x);
+        }
+
+    }
+    public void TransitionCamera() {
+        this.GetComponent<Animator>().enabled = false;
+        this.transform.parent = body;
+        this.transform.localPosition = Vector3.zero;
+        this.transform.localPosition = new Vector3(0, 0.838f, 0.233f); //good placement for the player cam
+        gameStarted = true;
+    }
+}
---
+++ Reflection/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed; //player speed
+    [SerializeField] private float gravity = -9.81f; //player gravity amount
+    [SerializeField] private Transform origin;
+    [SerializeField] private float jumpHeight = 3f;
+    public bool canMove = true;
+    Vector3 velocity;
+    public CharacterController controller;
+    private float groundDistance = 0.4f;
+    [SerializeField] bool isGrounded;
+    public LayerMask groundMask;
+    private void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update() {
+        if (canMove) {
+            isGrounded = Physics.CheckSphere(origin.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y <= 0) {
+                velocity.y = -2f;
+            }
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * Time.deltaTime * speed);
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+                //jump
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    
+}

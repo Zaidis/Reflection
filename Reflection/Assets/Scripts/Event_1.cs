c655e332c24b14b10a5a81d6bcd8735950423257
Zaidis <chewybinx@gmail.com>
2021-08-17 01:43:58 -0500

    easter egg

--- Reflection/Assets/Scripts/Event_1.cs
+++ Reflection/Assets/Scripts/Event_1.cs
@@ -6,9 +6,11 @@ public class Event_1 : TriggerEvent
 {
     //This event will spawn a box in the second room and play a sound effect
     public GameObject box;
+    public GameObject easterEgg;
     public AudioClip sound;
     public override void StartEvent() {
         box.SetActive(true);
+        easterEgg.SetActive(true);
         GameManager.instance.PlaySound(sound);
         Destroy(this.gameObject);
     }

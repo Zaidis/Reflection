fcf05882a24bee9b9a68e541b815005ab5be6dcf
Zaidis <chewybinx@gmail.com>
2021-08-19 01:34:18 -0500

    passcode works

--- Reflection/Assets/Scripts/GameManager.cs
+++ Reflection/Assets/Scripts/GameManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public List<int> keys = new List<int>();
+    public Locker[] lockers;
+
     public int batteryAmount { get; set; }
     private AudioSource source;
     [SerializeField] private string keyCode; //for the end
+    public TextMeshPro codeText;
     private void Awake() {
         if(instance == null) {
             instance = this;
@@ -21,6 +24,7 @@ public class GameManager : MonoBehaviour
 
     private void Start() {
         NewKeyCode();
+        LockerTextLocation();
     }
 
     public void NewKeyCode() {
@@ -34,6 +38,27 @@ public class GameManager : MonoBehaviour
     public string Code() {
         return keyCode;
     }
+
+    /// <summary>
+    /// This function chooses a random locker for the code to be in.
+    /// </summary>
+    public void LockerTextLocation() {
+        int rand = Random.Range(0, lockers.Length);
+        Locker theChosenLocker = lockers[rand];
+
+        codeText.transform.parent = theChosenLocker.GetComponent<Locker>().locationForCode.transform;
+        codeText.transform.localPosition = Vector3.zero;
+        if (theChosenLocker.GetComponent<Locker>().isLeft) {
+            //rotation = 0
+            codeText.transform.rotation = Quaternion.Euler(0, 0, 0);
+        } else {
+            //the locker is on the right side
+            codeText.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        codeText.text = keyCode.ToString();
+    }
+
     /// <summary>
     /// Checks if you have the key to open the door.
     /// </summary>
---
+++ Reflection/Assets/Scripts/Locker.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker : MonoBehaviour
+{
+
+    public bool isLeft; //if its left, rotation = 0
+    public Transform locationForCode;
+
+
+}
